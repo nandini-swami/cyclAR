06bf9d8e1e2c40445ccea1205c506e3a0c960753
nandini-swami <nandini.swami.18@gmail.com>
2025-11-04 16:17:58 -0500

    Initial Commit

---
+++ cyclAR/cyclARApp.swift
@@ -0,0 +1,17 @@
+//
+//  cyclARApp.swift
+//  cyclAR
+//
+//  Created by Nandini Swami on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct cyclARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cyclARTests/cyclARTests.swift
@@ -0,0 +1,17 @@
+//
+//  cyclARTests.swift
+//  cyclARTests
+//
+//  Created by Nandini Swami on 11/4/25.
+//
+
+import Testing
+@testable import cyclAR
+
+struct cyclARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
